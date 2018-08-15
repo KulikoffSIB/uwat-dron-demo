48d1a3be8b2a87b34f04b9e72305262ff1f805af
KulikovASe <KulikovASe@sibintek.ru>
2018-08-15 18:06:49 +0300

    рекативная форма регистрации и её верстка

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,8 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { RegistrationComponent } from './registration/registration.component';
+import {ReactiveFormsModule} from '@angular/forms';
+import {HttpClientModule} from '@angular/common/http';
 
 @NgModule({
   declarations: [
@@ -10,7 +12,9 @@ import { RegistrationComponent } from './registration/registration.component';
     RegistrationComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, FormGroup, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-registration',
@@ -6,10 +7,28 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./registration.component.css']
 })
 export class RegistrationComponent implements OnInit {
+  registration: FormGroup;
 
-  constructor() { }
+  constructor(
+    private fb: FormBuilder
+  ) {
+    this.registration = fb.group({
+      'lastname': [null, Validators.compose([Validators.required])],
+      'firstname': [null, Validators.compose([Validators.required])],
+      'middlename': [null, Validators.compose([Validators.required])],
+      'company': [null, Validators.compose([Validators.required])],
+      'post': [null, Validators.compose([Validators.required])],
+      'phone': [null, Validators.compose([Validators.required])],
+      'email': [null, Validators.compose([Validators.required, Validators.email])],
+    });
+  }
 
   ngOnInit() {
+  }
+
+  onSubmit() {
+    console.log(this.registration.valid);
+    console.log(this.registration.controls.name.value);
   }
 
 }
